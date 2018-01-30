05cf25e9f527af2c39bfc32fbb01315df73d7593
Malayandi Palaniappan <andypalan@Malayandis-MacBook-Pro.local>
2018-01-30 13:52:45 +0800

    Fixed bugs in check_win

---
+++ test.py
@@ -0,0 +1,9 @@
+from main import game
+
+assert game(rows=4, cols=4, n=3, plays=(0, 1, 0, 1, 0)) == "o" # end-to-end for vertical win
+
+assert game(rows=4, cols=4, n=3, plays=(0, 0, 1, 1, 2)) == "o"  # end-to-end for horizontal win
+
+assert game(rows=4, cols=4, n=3, plays=(0, 1, 1, 1, 2, 2, 2)) == "o"  # end-to-end for horizontal win
+
+assert game(rows=4, cols=4, n=3, plays=(2, 1, 1, 1, 0, 0, 0)) == "o"  # end-to-end for horizontal win
